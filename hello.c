4152d2878929378038f56b06a6f9236e76387242
La Van Thien <thienlv@dasannetworks.com>
2017-04-20 17:55:07 +0900

    Add sum operation

--- hello.c
+++ hello.c
@@ -3,10 +3,13 @@
 int main ()
 {
     int i = 0;
+    int a = 4;
+    int b = 5;
     
     /* Fix some bug in dev branch */
     printf ("This is dev branch\n");
     printf ("Hello GitHub community. My name is thienlv\n");
+    printf ("Some %d + %d = %d\n", a, b, a + b);
 
     return 0;
 }
