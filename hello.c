b862038b374bead0182b7cc1714227028f148b68
thienlv <lavanthiendtvt@gmail.com>
2017-04-23 13:05:07 +0700

    Comment meaningless line

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@ int main ()
     /* Fix some bug in dev branch */
     printf ("This is dev branch\n");
     printf ("Hello GitHub community. My name is thienlv\n");
-    printf ("This is from thienlv in devser Hong Mai\n");
+    //printf ("This is from thienlv in devser Hong Mai\n");
 
     return 0;
 }
